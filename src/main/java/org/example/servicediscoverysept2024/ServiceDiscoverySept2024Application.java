b15b052dc38b15d7aa1364fb9df46e20133598e2
khannaanurag1 <khannaanurag1@gmail.com>
2024-11-12 23:31:29 +0530

    Draft

---
+++ src/main/java/org/example/servicediscoverysept2024/ServiceDiscoverySept2024Application.java
@@ -0,0 +1,13 @@
+package org.example.servicediscoverysept2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoverySept2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoverySept2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/servicediscoverysept2024/ServiceDiscoverySept2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicediscoverysept2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoverySept2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
